ec4b99829bd7b299742d01cc86ef6a836a312cc8
JesterXL <jessew@webappsolution.com>
2013-12-05 22:15:18 -0500

    unit test suite engaged

---
+++ TicTacToe/web/tests/UnitTests.dart
@@ -0,0 +1,40 @@
+library	tictactoeunittests;
+
+import 'dart:html';
+
+import 'package:unittest/unittest.dart';
+import 'package:unittest/html_enhanced_config.dart';
+
+import '../com/jessewarden/tictactoegame/tictactoegamelib.dart';
+import '../com/jessewarden/statemachine/statemachinelib.dart';
+
+part 'TestStateMachine.dart';
+
+class UnitTests
+{
+	UnitTests()
+	{
+		useHtmlEnhancedConfiguration();
+	}
+
+	void onTestResult(TestCase testCase)
+	{
+		write("${testCase.result} ${testCase.currentGroup}");
+	}
+
+	void write(String message)
+	{
+		document.querySelector('#status').innerHtml	= message;
+	}
+
+	void run()
+	{
+		new	TestStateMachine().run();
+	}
+}
+
+void main()
+{
+	// start this mug
+	new	UnitTests().run();
+}
